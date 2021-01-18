8746ecc3c76ddd4ee51f56298b112711166a0bf1
amuedojulian <amuedojulian05@gmail.com>
2021-01-17 22:51:13 -0300

    Add Entities

--- src/main/java/com/pontointeligente/pontointeligente/model/Empressa.java
+++ src/main/java/com/pontointeligente/pontointeligente/model/Empressa.java
@@ -1,4 +1,99 @@
 package com.pontointeligente.pontointeligente.model;
 
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "empresa")
 public class Empressa {
+
+    private Long id;
+    private String razonSocial;
+    private String cnpj;
+    private Date dataCriacao;
+    private Date dataAtualizacao;
+    private List<Funcionario> funcionarios;
+
+    public Empressa() {
+    }
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "razon_social", nullable = false)
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    @Column(name = "cnpj", nullable = false)
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    @Column(name = "data_criacao", nullable = false)
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    @Column(name = "data_atualizacao", nullable = false)
+    public Date getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(Date dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    @OneToMany(mappedBy = "empresa", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    public List<Funcionario> getFuncionarios(){
+        return funcionarios;
+    }
+
+    public void setFuncionarios(List<Funcionario> funcionarios) {
+        this.funcionarios = funcionarios;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        dataAtualizacao = new Date();
+    }
+
+    @PrePersist
+    public void prePresist() {
+        final Date atual = new Date();
+        dataCriacao = atual;
+        dataAtualizacao = atual;
+    }
+
+    @Override
+    public String toString() {
+        return "Empressa{" +
+                "id=" + id +
+                ", razonSocial='" + razonSocial + '\'' +
+                ", cnpj='" + cnpj + '\'' +
+                ", dataCriacao=" + dataCriacao +
+                ", dataAtualizacao=" + dataAtualizacao +
+                ", funcionarios=" + funcionarios +
+                '}';
+    }
 }
---
+++ src/main/resources/db/migration/mysql/V1__init.sql
@@ -0,0 +1,70 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+--
+-- Base de datos: `ponto-inteligente`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empresa`
+--
+
+DROP TABLE IF EXISTS `empresa`;
+CREATE TABLE IF NOT EXISTS `empresa` (
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `razon_social` varchar(20) NOT NULL,
+    `cnpj` varchar(255) NOT NULL,
+    `data_criacao` datetime NOT NULL,
+    `data_atualizacao` datetime NOT NULL,
+    PRIMARY KEY (`id`)
+    ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `funcionario`
+--
+
+DROP TABLE IF EXISTS `funcionario`;
+CREATE TABLE IF NOT EXISTS `funcionario` (
+    `id` int(10) NOT NULL AUTO_INCREMENT,
+    `nome` varchar(255) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `senha` bigint(20) NOT NULL,
+    `cpf` varchar(255) NOT NULL,
+    `valor_hora` decimal(19,2) DEFAULT NULL,
+    `qtd_horas_trabalho_dia` float DEFAULT NULL,
+    `qtd_horas_almoco` float DEFAULT NULL,
+    `perfil` varchar(255) NOT NULL,
+    `data_criacao` datetime NOT NULL,
+    `data_atualizacao` datetime NOT NULL,
+    `empresa_id` bigint(20) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `empresa_id` (`empresa_id`)
+    ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `lancamento`
+--
+
+DROP TABLE IF EXISTS `lancamento`;
+CREATE TABLE IF NOT EXISTS `lancamento` (
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `date` datetime NOT NULL,
+    `descricao` varchar(255) NOT NULL,
+    `localicacao` varchar(255) NOT NULL,
+    `data_criacao` datetime NOT NULL,
+    `data_atualizacao` datetime NOT NULL,
+    `tipo` varchar(255) NOT NULL,
+    `funcionario_id` bigint(20) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `funcionario_id` (`funcionario_id`)
+    ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+COMMIT;
+
